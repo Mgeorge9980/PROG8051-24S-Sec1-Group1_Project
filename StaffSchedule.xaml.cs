7f258bc2e98cc7b399d475da2cbaa693b0e2cc66
Shilpa Gopi <Sgopi3284@conestogac.on.ca>
2024-07-30 14:52:27 -0400

    Staff schedule page added and busg fixed

---
+++ StaffSchedule.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace StudioManagement
+{
+    public partial class StaffScheduleWindow : Window
+    {
+        public StaffScheduleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MinimizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeWindow_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SetButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get selected staff name and day
+            var selectedStaff = staffNameComboBox.SelectedItem as ComboBoxItem;
+            var selectedDay = dayComboBox.SelectedItem as ComboBoxItem;
+
+            if (selectedStaff != null && selectedDay != null)
+            {
+                string staffName = selectedStaff.Content.ToString();
+                string dayName = selectedDay.Content.ToString();
+
+                // Here you can implement the logic for setting the staff schedule
+                MessageBox.Show($"Schedule set for {staffName} on {dayName}.", "Schedule Set", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please select both staff and day.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
